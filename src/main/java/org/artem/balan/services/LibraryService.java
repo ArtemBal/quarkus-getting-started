f03a09bcd5560c0b02023a62be7998908554abc8
Artem Balan <a.s.balan96@gmail.com>
2024-03-20 23:36:43 +0200

    fix bugs

--- src/main/java/org/artem/balan/services/LibraryService.java
+++ src/main/java/org/artem/balan/services/LibraryService.java
@@ -1,11 +1,11 @@
 package org.artem.balan.services;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.artem.balan.model.Book;
 import org.artem.balan.repositories.BookRepository;
 
-import javax.inject.Inject;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
